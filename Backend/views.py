664109f05bc9619b040977c3f6ced1a3375ce7b4
jakubmisiak <jakubmisiak@icloud.com>
2021-05-25 17:01:22 +0200

    final

--- Backend/views.py
+++ Backend/views.py
@@ -1,13 +1,14 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
-
+from AIProjektZaliczeniowy.settings import MEDIA_URL
 from Backend.forms import PicForm
 from Backend.detect_face_image import detect_image
 from Backend.models import Pic
 
 
 def index(request):
+    media_url = MEDIA_URL
     form = PicForm()
     pics = Pic.objects.all().order_by('-id')
     posts_paginator = Paginator(pics, 5)
@@ -17,11 +18,13 @@ def index(request):
         form = PicForm(request.POST, request.FILES)
         if form.is_valid():
             new_pic = form.save()
+            print(type(new_pic.pic))
             detect_image(new_pic.pic)
         return redirect('index')
 
     context = {
         'page': page,
-        'form': form
+        'form': form,
+        'MEDIA_URL': media_url,
     }
     return render(request, 'index.html', context)
